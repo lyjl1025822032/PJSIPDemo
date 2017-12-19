af386ebedaac25d590dec6ca4347a3f5f5df15ce
lyjl1025822032 <m18272733524@163.com>
2017-12-19 09:46:30 +0800

    Initial Commit

---
+++ PJSIPDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PJSIPDemo
+//
+//  Created by yao on 2017/12/19.
+//  Copyright © 2017年 王智垚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PJSIPDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PJSIPDemo
+//
+//  Created by yao on 2017/12/19.
+//  Copyright © 2017年 王智垚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
